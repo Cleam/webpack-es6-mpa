fc2c5fb37e636280e962a5587cb693ca91b7ae4a
lizhigao <lizhigao@021.com>
2021-01-22 12:08:13 +0800

    update plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,13 @@
 module.exports = {
-  root: true,
   // 环境配置（根据实际情况做取舍）
   env: {
-    browser: true,
-    node: true,
-    commonjs: true,
-    es6: true,
-    jquery: true,
+    jquery: true
   },
   globals: {
     PRODUCTION: true,
     PREPRODUCTION: true,
-    DEVELOPMENT: true,
+    DEVELOPMENT: true
   },
-  // 继承eslint的默认配置和prettier的配置。
-  plugins: ['xmfe'],
-  extends: ['plugin:xmfe/recommended'],
-  // // 支持babel的使用
-  // parser: 'babel-eslint',
-  // parserOptions: {
-  //   // 支持es6模块语法
-  //   sourceType: 'module',
-  // }
+  plugins: ['xyz'],
+  extends: ['plugin:xyz/common']
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   singleQuote: true,
-  printWidth: 120,
+  printWidth: 100,
   tabWidth: 2,
   semi: true
 };
